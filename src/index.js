2f6a8c67c7b68e913a0e67de42d78503ef48ee66
Insikynwa <pavel.churkin@mail.ru>
2021-03-14 20:54:27 +0300

    feat: implement function reverse-int

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
-module.exports = function reverse (n) {
-  
-}
+module.exports = function reverse(n) {
+    let newNumber = 0;
+    newNumber = Math.abs(n).toString().split("").reverse().join("");
+    return Number(newNumber);
+};
